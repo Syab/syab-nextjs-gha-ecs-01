9a4af1db12b115709b26c53e3df1631da0944190
syab <syabster.phang18@gmail.com>
2021-09-22 00:04:10 +0800

    testing terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "scan_images_on_push" {
+  type        = bool
+  description = "Indicates whether images are scanned after being pushed to the repository (true) or not (false)"
+  default     = true
+}
+
+variable "max_image_count" {
+  type        = number
+  description = "How many Docker Image versions AWS ECR will store"
+  default     = 500
+}
+
+variable "image_names" {
+  type        = list(string)
+  default     = []
+  description = "List of Docker local image names, used as repository names for AWS ECR "
+}
+
+variable "image_tag_mutability" {
+  type        = string
+  default     = "MUTABLE"
+  description = "The tag mutability setting for the repository. Must be one of: `MUTABLE` or `IMMUTABLE`"
+}
